5c64432567c894e671dece4db52b4044f3201552
jadidihosein68 <jadidi.hosein@gmail.com>
2023-04-30 18:59:42 +0800

    add footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EnglishLearnerCardComponent } from './english-learner-card/english-learner-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    EnglishLearnerCardComponent
+    FooterComponent,
+    EnglishLearnerCardComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [NavbarComponent,
+  exports: [
+    NavbarComponent,
+    FooterComponent,
     EnglishLearnerCardComponent]
 })
 export class SharedModule { }
