b7734efde39d8a0ff7252b388b9e14b59c4d7f7f
jadidihosein68 <jadidi.hosein@gmail.com>
2023-05-01 22:13:44 +0800

    add a better design

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Plan {
+  title: string;
+  price: string;
+  features: string[];
+}
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  plans: Plan[] = [
+    {
+      title: 'Free Plan',
+      price: 'Free',
+      features: [
+        'Access to flashcards',
+        'Basic pronunciation'
+      ]
+    },
+    {
+      title: 'Basic Plan',
+      price: '$9.99/mo',
+      features: [
+        'All Free Plan features',
+        'Upload custom flashcards',
+        'AI pronunciation for PTE and IELTS'
+      ]
+    },
+    {
+      title: 'Advanced Plan',
+      price: '$19.99/mo',
+      features: [
+        'All Basic Plan features',
+        'Unlimited custom flashcards',
+        'Priority customer support'
+      ]
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
