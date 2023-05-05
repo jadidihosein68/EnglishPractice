7ac5285bd723cf618e3092c17284d07e6760509d
jadidihosein68 <jadidi.hosein@gmail.com>
2023-05-05 15:44:58 +0800

    add search component in card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EnglishLearnerCardComponent } from './english-learner-card/english-learner-card.component';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { EnglishLearnerCardHeaderComponent } from './english-learner-card-header/english-learner-card-header.component';
 
 
 
 
 @NgModule({
   declarations: [    
-    EnglishLearnerCardComponent, CourseCardComponent,
+    EnglishLearnerCardComponent, CourseCardComponent, EnglishLearnerCardHeaderComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [    
-    EnglishLearnerCardComponent, CourseCardComponent]
+    EnglishLearnerCardComponent, CourseCardComponent, EnglishLearnerCardHeaderComponent]
 })
 export class SharedModule { }
