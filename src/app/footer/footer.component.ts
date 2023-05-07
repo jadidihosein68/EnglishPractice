057f399c1a4fb90d1dc0655df76628dc95aab88f
jadidihosein68 <jadidi.hosein@gmail.com>
2023-05-07 12:27:03 +0800

    update component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { environment } from '../../environments/environments';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  canShowLogin?: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.canShowLogin = !environment.production;
+  }
 
 }
