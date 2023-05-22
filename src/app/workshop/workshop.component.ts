e66200b29cfa39cdeaa534cb06ea8b70c1bb8047
jadidihosein68 <jadidi.hosein@gmail.com>
2023-05-22 17:45:43 +0800

    update woprkshop

--- src/app/workshop/workshop.component.ts
+++ src/app/workshop/workshop.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FlashCardSetService } from '../services/FlashCardSetService';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+
 
 @Component({
   selector: 'app-workshop',
@@ -8,11 +11,30 @@ import { FlashCardSetService } from '../services/FlashCardSetService';
 })
 export class WorkshopComponent {
 
+  cols: number;
 
-  constructor(private flashCardSetService: FlashCardSetService) {}
+  constructor(private flashCardSetService: FlashCardSetService , private breakpointObserver: BreakpointObserver) {}
 
 
    ngOnInit() {
+
+
+
+
+
+    this.breakpointObserver.observe([
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.cols = 1;  // use one column on small screens
+      } else {
+        this.cols = 5;  // use three columns on larger screens
+      }
+    });
+
+
+
+
     
     this.flashCardSetService.getFlashCardSets().subscribe(
       data => {
